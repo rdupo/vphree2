a263bcfb4207c393fac39b2e2087512f07489ec6
rdupo <67809219+rdupo@users.noreply.github.com>
2023-07-02 12:31:49 -0500

    added price and pagination

--- components/card.js
+++ components/card.js
@@ -8,12 +8,16 @@ class card extends HTMLElement {
     connectedCallback() {
     	let img = this.attributes.img.value;
     	let id = this.attributes.id.value;
+    	let atts = this.attributes.atts.value;
     	let price = this.attributes.price.value;
     	cardTemplate.innerHTML = `
     		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Silkscreen|Ubuntu+Mono|Roboto+Mono">
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 			<link rel="stylesheet" type="text/css" href="style.css">
-    		<div class="card-wrapper brite my-2 black-bg white-txt d-inline-block">
+    		<div class="card-wrapper brite my-2 black-bg white-txt d-inline-block" 
+    			 data-price="${price}" 
+    			 data-atts="${atts}"
+    			 id="${id}">
 				<div class="img-wrapper v3-bg">
 					<img class="w-100" src="${img}" loading="lazy">
 				</div> 
