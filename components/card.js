0b4655364a55be638c28cd3df07dbac1d4b18049
rdupo <67809219+rdupo@users.noreply.github.com>
2023-07-11 21:20:06 -0500

    move from modal to page

--- components/card.js
+++ components/card.js
@@ -14,10 +14,11 @@ class card extends HTMLElement {
     		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Silkscreen|Ubuntu+Mono|Roboto+Mono">
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 			<link rel="stylesheet" type="text/css" href="style.css">
-    		<div class="card-wrapper brite my-2 black-bg white-txt d-inline-block" 
+    		<a class="card-wrapper brite my-2 black-bg white-txt d-inline-block sans-underline" 
     			 data-price="${price}" 
     			 data-atts="${atts}"
-    			 id="${id}">
+    			 id="${id}"
+    			 href="/vphree2/phunk#${id}">
 				<div class="img-wrapper v3-bg">
 					<img class="w-100" src="${img}" loading="lazy">
 				</div> 
@@ -25,7 +26,7 @@ class card extends HTMLElement {
 					<p class="phunk-id mb-0 v3-txt">#${id}</p>
 					<h4 class="phunk-price mb-1">${price}Ξ</h4>
 				</div>
-			</div>
+			</a>
 		`
 
         const shadowRoot = this.attachShadow({ mode: 'open' });
